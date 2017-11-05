8e182684a6368ea5443a9eeb31fc2a4401c48188
Alexandre VINCENT <vincentale@eisti.eu>
2017-11-05 23:10:25 +0100

    Login through Firebase Authentication
    ### Added :
     - A new LoginActivity with a email + password form
     - Associated resources
     - FirebaseAuthenticator and  Listener
     - A Method to sign in firebase
     - A TextView on HomeActivity to display the current username (displays null though...)

--- leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/HomeActivity.java
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/HomeActivity.java
@@ -1,13 +1,33 @@
 package com.example.vincentale.leafguard_core;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static final String TAG = "HomeActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Resources res = getResources();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+
+        TextView helloText = (TextView) findViewById(R.id.helloText);
+        if (mUser != null) {
+            helloText.setText(res.getString(R.string.hello_name, mUser.getDisplayName()));
+        } else {
+            Log.i(TAG, "You shouldn't be anonymous on this activity !");
+        }
     }
 }
