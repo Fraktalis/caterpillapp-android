26ad639a9d57e963a5ed7f27a92ba3a253c5f357
Alexandre VINCENT <vincentale@eisti.eu>
2017-11-21 18:55:34 +0100

    ### Added :
     - a datepicker pop-up for oak selection
     - a clickable editText to invoke the pop up

--- leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/OakFormActivity.java
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/OakFormActivity.java
@@ -1,13 +1,25 @@
 package com.example.vincentale.leafguard_core;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class OakFormActivity extends AppCompatActivity {
+
+    private Calendar myCalendar = Calendar.getInstance();
+    private EditText datePickerInput;
+    DatePickerDialog.OnDateSetListener date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,43 @@ public class OakFormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_oak_form);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        datePickerInput = (EditText) findViewById(R.id.datePicker);
+        datePickerInput.setKeyListener(null); //To make it uneditable ! Java :D
+
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+                datePickerInput.clearFocus();
+            }
+
+        };
+
+        final Context activityContext = this;
+        datePickerInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    new DatePickerDialog(activityContext, date, myCalendar
+                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "yyyy/MM/dd"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        datePickerInput.setText(sdf.format(myCalendar.getTime()));
     }
 
 }
