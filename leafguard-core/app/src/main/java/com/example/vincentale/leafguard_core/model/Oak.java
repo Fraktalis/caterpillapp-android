fb69ec76dd320b7a27c311610cb6afc78cea2df7
Alexandre VINCENT <vincentale@eisti.eu>
2017-12-06 16:57:49 +0100

    Added models for Oak and Observation

---
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/model/Oak.java
@@ -0,0 +1,80 @@
+package com.example.vincentale.leafguard_core.model;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by vincentale on 06/12/17.
+ */
+
+public class Oak {
+
+    private Oak() {
+
+    }
+
+    public Oak(@NonNull User user, float longitude, float latitude) {
+        setUid(user.getUid() + "_" + longitude + "_" + latitude);
+        setLongitude(longitude);
+        setLatitude(latitude);
+    }
+
+    private String uid;
+
+    private float longitude;
+
+    private float latitude;
+
+    private float oakCircumference;
+
+    private float oakHeight;
+
+    private long installationDate;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getOakCircumference() {
+        return oakCircumference;
+    }
+
+    public void setOakCircumference(float oakCircumference) {
+        this.oakCircumference = oakCircumference;
+    }
+
+    public float getOakHeight() {
+        return oakHeight;
+    }
+
+    public void setOakHeight(float oakHeight) {
+        this.oakHeight = oakHeight;
+    }
+
+    public long getInstallationDate() {
+        return installationDate;
+    }
+
+    public void setInstallationDate(long installationDate) {
+        this.installationDate = installationDate;
+    }
+}
