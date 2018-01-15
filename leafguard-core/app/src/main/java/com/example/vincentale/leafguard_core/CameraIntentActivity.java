de1fa20de21a6656d63d274f5229a04b8a0d7f38
Marguerite-Charlotte PERON <peronmargu@eisti.eu>
2018-01-15 19:20:41 +0100

    -  Upload the image on firebase

--- leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/CameraIntentActivity.java
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/CameraIntentActivity.java
@@ -1,14 +1,15 @@
 package com.example.vincentale.leafguard_core;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -19,6 +20,13 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.vincentale.leafguard_core.util.ImageAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,14 +36,20 @@ import java.util.Date;
 public class CameraIntentActivity extends Activity {
     // TODO: Reduire le choix des settings
     // TODO : Action depuis la recycle view
-    //TODO : Firebase
+    // TODO : Récupérer de firebase
+    // TODO : Envoyer vers firebase
+    // TODO : Faire une miniature qui se créer sur firebase et cest elle qui sera telecharger
 
     private static final int ACTIVITY_START_CAMERA_APP = 0;
     private ImageView mPhotoCapturedImageView;
     private String mImageFileLocation = "";
-    private String GALLERY_LOCATION = "image gallery";
+    private String GALLERY_LOCATION = "Caterpilapp image gallery";
     private File mGalleryFolder;
-
+    Uri selectedImage;
+    ProgressDialog progressDialog;
+    UploadTask uploadTask;
+    FirebaseStorage storage;
+    StorageReference storageRef,imageRef;
     private RecyclerView mRecyclerView;
 
     @Override
@@ -52,7 +66,10 @@ public class CameraIntentActivity extends Activity {
         if (imageAdapter.getItemCount() >= 3 )
             button.setEnabled(false);
         mRecyclerView.setAdapter(imageAdapter);
-
+        //accessing the firebase storage
+        storage = FirebaseStorage.getInstance();
+        //creates a storage reference
+        storageRef = storage.getReference();
     }
 
     @Override
@@ -88,20 +105,15 @@ public class CameraIntentActivity extends Activity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-
+        selectedImage = Uri.fromFile(photoFile);
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, selectedImage);
         startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
     }
 
     protected void onActivityResult (int requestCode, int resultCode, Intent data) {
         if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
             Toast.makeText(this, "Picture taken successfully", Toast.LENGTH_SHORT).show();
-            // Bundle extras = data.getExtras();
-            // Bitmap photoCapturedBitmap = (Bitmap) extras.get("data");
-            // mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
-            // Bitmap photoCapturedBitmap = BitmapFactory.decodeFile(mImageFileLocation);
-            // mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
-            // setReducedImageSize();
+            uploadImage();
             RecyclerView.Adapter newImageAdapter = new ImageAdapter(mGalleryFolder);
             mRecyclerView.swapAdapter(newImageAdapter, false);
 
@@ -129,23 +141,46 @@ public class CameraIntentActivity extends Activity {
 
     }
 
-    void setReducedImageSize() {
-        int targetImageViewWidth = mPhotoCapturedImageView.getWidth();
-        int targetImageViewHeight = mPhotoCapturedImageView.getHeight();
 
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
-        int cameraImageWidth = bmOptions.outWidth;
-        int cameraImageHeight = bmOptions.outHeight;
-
-        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth, cameraImageHeight/targetImageViewHeight);
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inJustDecodeBounds = false;
-
-        Bitmap photoReducedSizeBitmp = BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
-        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmp);
-
-
+    public void uploadImage() {
+        //create reference to images folder and assing a name to the file that will be uploaded
+        imageRef = storageRef.child("images/"+selectedImage.getLastPathSegment());
+        //creating and showing progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMax(100);
+        progressDialog.setMessage("Uploading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+        //starting upload
+        uploadTask = imageRef.putFile(selectedImage);
+        // Observe state change events such as progress, pause, and resume
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                //sets and increments value of progressbar
+                progressDialog.incrementProgressBy((int) progress);
+            }
+        });
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Toast.makeText(CameraIntentActivity.this,"Error in uploading!",Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Toast.makeText(CameraIntentActivity.this,"Upload successful",Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+                //showing the uploaded image in ImageView using the download url
+                //Picasso.with(CameraIntentActivity.this).load(downloadUrl).into(imageView);
+            }
+        });
     }
 }
