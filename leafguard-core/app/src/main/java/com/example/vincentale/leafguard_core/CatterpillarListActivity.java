ccb04e65f5fb4fe532af7aa8b91eb9c04f6939fc
lecontemat <lecontemat@eisti.eu>
2018-01-22 00:32:25 +0100

    adding files but not working

---
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/CatterpillarListActivity.java
@@ -0,0 +1,39 @@
+package com.example.vincentale.leafguard_core;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.vincentale.leafguard_core.dummy.DummyContent;
+import com.example.vincentale.leafguard_core.model.Catterpillar;
+import com.example.vincentale.leafguard_core.view.CatterpillarListAdapter;
+
+import java.util.List;
+
+public class CatterpillarListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cattarpillar_list);
+        recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        List<Catterpillar> input= DummyContent.ITEMS;
+        mAdapter =new CatterpillarListAdapter(input);
+
+
+        recyclerView.setAdapter(mAdapter);
+
+
+
+    }
+}
