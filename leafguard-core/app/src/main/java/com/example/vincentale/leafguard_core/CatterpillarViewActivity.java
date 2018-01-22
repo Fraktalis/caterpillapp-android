078eb44eadc2f81760134aa99fa0667d0a0f0896
lecontemat <lecontemat@eisti.eu>
2018-01-22 10:33:51 +0100

    edit the on catterpillar for attack by mammals to attacked by mammals

--- leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/CatterpillarViewActivity.java
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/CatterpillarViewActivity.java
@@ -1,10 +1,13 @@
 package com.example.vincentale.leafguard_core;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -60,6 +63,8 @@ public class CatterpillarViewActivity extends AppCompatActivity {
         catterpillarName.setText(iD);
 
         nbWound.setText(String.valueOf(item.getWounds()));
+
+
         isMissing.setChecked(item.isCatterpillarMissing());
         if (isMissing.isChecked())
         {
