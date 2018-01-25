60ee1ee6f9a130a91a990fcf0aebf9957efade8a
Marguerite-Charlotte PERON <peronmargu@eisti.eu>
2018-01-25 11:49:34 +0100

    - Download of the images - Disable photo if there are 3 photo from firebase

---
+++ leafguard-core/app/src/main/java/com/example/vincentale/leafguard_core/model/ImageCaterpillar.java
@@ -0,0 +1,20 @@
+package com.example.vincentale.leafguard_core.model;
+
+/**
+ * Created by Administrator on 24/01/2018.
+ */
+
+public class ImageCaterpillar {
+    public String imageURL;
+
+    public ImageCaterpillar() {
+    }
+
+    public ImageCaterpillar(String url) {
+        this.imageURL = url;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+}
